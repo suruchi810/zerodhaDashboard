6259802e51e95858fec126adb2f19c89bdee7ef4
suruchi810 <161755548+suruchi810@users.noreply.github.com>
2025-01-04 17:39:01 +0530

    Update Positions.jsx

--- src/components/Positions.jsx
+++ src/components/Positions.jsx
@@ -7,7 +7,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3002/allPositions/').then((res)=>{
+    axios.get('https://zerodhabackend-l8sl.onrender.com/allPositions/').then((res)=>{
       setAllPositions(res.data);
     })
   }, []);
